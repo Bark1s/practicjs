3fc25d9e2c7405a00e93aab3e5e6ea7fd4273268
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-01-14 15:18:03 +0200

    task-2

--- js/lesson1.js
+++ js/lesson1.js
@@ -1,5 +1,16 @@
-let username = "Галина";
-let age = 35;
-let subject = "JAVASCRIPT";
+// let username = "Галина";
+// let age = 35;
+// let subject = "JAVASCRIPT";
 
-console.log(`ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${username}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${subject}`);
+// console.log(`ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${username}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${subject}`);
+
+
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const name = prompt("Введіть своє імʼя");
+// const email = prompt("Введіть свою пошту");
+// const tel = prompt("Введіть свій телефон");
+
+// alert(`КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ ${email} ПОЧТУ І ${tel} ТЕЛЕФОН`);
