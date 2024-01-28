fd1ca881a70b9bab53a315a9d1b8f4fe88691d2d
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-01-28 16:46:41 +0200

    task 3 obj

--- js/lesson2.js
+++ js/lesson2.js
@@ -253,3 +253,32 @@
 // }
 
 // console.log(summary(salaries));
+
+// ЗАДАЧА 3
+// Створи обєкт calculator(калькулятор) з 3-ма методами:
+// read()- запитує 2 значення у юзера і зберігає їх як значення обєкта;
+// sum() -повертає суму збережених значень; 
+// mult() -перемножує значення збережені і повертає результат;
+
+//  const calculator = {
+//  a: null,
+//  b: null,
+
+//      read() {
+//          this.a = prompt("give a number 1")
+//          this.b = prompt("number 2")
+//   },
+
+//      sum() {
+//       return Number(this.a) + Number(this.b)
+//   },
+
+//      mult() {
+//       return this.a * this.b
+//   },
+// };
+
+
+// calculator.read();
+// console.log(`sum`, calculator.sum());
+// console.log(`mult`, calculator.mult());
