773ba9d1a393788f76b6b55bf71e6b48ad9a45b4
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-02-11 16:32:35 +0200

    task - class

--- js/lesson3.js
+++ js/lesson3.js
@@ -230,3 +230,31 @@
 //     .reduce((acc, ratin) => acc + ratin, 0) / ratingFour.length;
 
 // console.log(ratinArr);
+
+// Напишіть клас User для створення користувача з наступними властивостями:
+// username - імя, рядок
+// age - вік, число
+// numberOfPosts - кількість постів, число
+// Клас очікує один параметр - об'єкт налаштувань з однойменними властивостями.
+// Добавте метод getInfo(), який повертає рядок:
+// User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
+
+// class User {
+//   constructor({ username, age, numberOfPosts }) {
+//     this.username = username;
+//     this.age = age;
+//     this.numberOfPosts = numberOfPosts;
+//   }
+
+//   getInfo() {
+//     return `User ${this.username} is ${this.age} years old and has ${this.numberOfPosts} posts.`;
+//   };
+// }
+
+// const newUser = new User({
+//   username: "Alex",
+//   age: 65,
+//   numberOfPosts: 15,
+// });
+
+// console.log(newUser);
