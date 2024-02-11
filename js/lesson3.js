0b634c5bd7584623fe74d5e81be3fc06b33a0a43
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-02-11 15:49:31 +0200

    trata

--- js/lesson3.js
+++ js/lesson3.js
@@ -36,93 +36,93 @@
 // МАЄМО МАСИВ, ПОТРІБНО:
 // 1)получити масив вчених які народились у 19ст;
 
-const scientists = [
-    {
-      name: "Albert",
-      surname: "Einstein",
-      born: 1879,
-      dead: 1955,
-      id: 1,
-    },
-    {
-      name: "Isaac",
-      surname: "Newton",
-      born: 1643,
-      dead: 1727,
-      id: 2,
-    },
-    {
-      name: "Galileo",
-      surname: "Galilei",
-      born: 1564,
-      dead: 1642,
-      id: 3,
-    },
-    {
-      name: "Marie",
-      surname: "Curie",
-      born: 1867,
-      dead: 1934,
-      id: 4,
-    }, 
-    {
-      name: "Johannes",
-      surname: "Kepler",
-      born: 1571,
-      dead: 1630,
-      id: 5,
-    },
-    {
-      name: "Nicolaus",
-      surname: "Copernicus",
-      born: 1473,
-      dead: 1543,
-      id: 6,
-    },
-    {
-      name: "Max",
-      surname: "Planck",
-      born: 1858,
-      dead: 1947,
-      id: 7,
-    },
-    {
-      name: "Katherine",
-      surname: "Blodgett",
-      born: 1898,
-      dead: 1979,
-      id: 8,
-    }, 
-    {
-      name: "Ada",
-      surname: "Lovelace",
-      born: 1815,
-      dead: 1852,
-      id: 9,
-    },
+// const scientists = [
+//     {
+//       name: "Albert",
+//       surname: "Einstein",
+//       born: 1879,
+//       dead: 1955,
+//       id: 1,
+//     },
+//     {
+//       name: "Isaac",
+//       surname: "Newton",
+//       born: 1643,
+//       dead: 1727,
+//       id: 2,
+//     },
+//     {
+//       name: "Galileo",
+//       surname: "Galilei",
+//       born: 1564,
+//       dead: 1642,
+//       id: 3,
+//     },
+//     {
+//       name: "Marie",
+//       surname: "Curie",
+//       born: 1867,
+//       dead: 1934,
+//       id: 4,
+//     },
+//     {
+//       name: "Johannes",
+//       surname: "Kepler",
+//       born: 1571,
+//       dead: 1630,
+//       id: 5,
+//     },
+//     {
+//       name: "Nicolaus",
+//       surname: "Copernicus",
+//       born: 1473,
+//       dead: 1543,
+//       id: 6,
+//     },
+//     {
+//       name: "Max",
+//       surname: "Planck",
+//       born: 1858,
+//       dead: 1947,
+//       id: 7,
+//     },
+//     {
+//       name: "Katherine",
+//       surname: "Blodgett",
+//       born: 1898,
+//       dead: 1979,
+//       id: 8,
+//     },
+//     {
+//       name: "Ada",
+//       surname: "Lovelace",
+//       born: 1815,
+//       dead: 1852,
+//       id: 9,
+//     },
   
-    {
-      name: "Lise",
-      surname: "Meitner",
-      born: 1878,
-      dead: 1968,
-      id: 11,
-    },
-    {
-      name: "Sarah E.",
-      surname: "Goode",
-      born: 1855,
-      dead: 1905,
-      id: 10,
-    },
-    {
-      name: "Hanna",
-      surname: "Hammarström",
-      born: 1829,
-      dead: 1909,
-      id: 12,
-    },
-  ];
+//     {
+//       name: "Lise",
+//       surname: "Meitner",
+//       born: 1878,
+//       dead: 1968,
+//       id: 11,
+//     },
+//     {
+//       name: "Sarah E.",
+//       surname: "Goode",
+//       born: 1855,
+//       dead: 1905,
+//       id: 10,
+//     },
+//     {
+//       name: "Hanna",
+//       surname: "Hammarström",
+//       born: 1829,
+//       dead: 1909,
+//       id: 12,
+//     },
+//   ];
 
 // const newScien = scientists.filter((scientist) => (scientist.born < 1900) && (scientist.born > 1800)).map((scientist) => `${scientist.name} ${scientist.surname}`)
  
@@ -162,3 +162,16 @@ const scientists = [
 //   (name) => name.name === "Albert" && name.surname === "Einstein"
 // );
 // console.log(findName.born);
+
+// Об'єкт: Масив цілих чисел.
+// Умова: Знайти суму всіх елементів масиву. Використати метод reduce для обчислення суми елементів.
+// arr = [1, 2, 3, 4, 5]
+
+// const sum = arr.reduce((total, num) => total + num, 0)
+// console.log(sum)
+
+// Об'єкт: Масив рядків.
+// Умова: Знайти кількість рядків, які мають довжину менше 5 символів. Використати метод filter для відфільтрування рядків за умовою довжини.
+// const stringsArray = ["apple", "banana", "kiwi", "grape", "pear"];
+
+// console.log(stringsArray.filter((word) => word.length < 5))
