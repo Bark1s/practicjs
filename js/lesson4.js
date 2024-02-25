d1d508d6b73c8f534c84050609e187d470ddb9c8
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-02-25 16:54:03 +0200

    add

--- js/lesson4.js
+++ js/lesson4.js
@@ -157,3 +157,66 @@
 //   taskList.append(listItem);
 //   form.reset();
 // });
+
+// ЗАДАЧА 4
+// ПОТРІБНО СТВОРИТИ Ф-ЦІЮ , ЯКА БУДЕ ШУКАТИ НАРЦИСА.ГОЛОВНА УМОВА НАРЦИСА ЗНАЮТЬ УСІ , НАРЦИС НЕ ЗНАЄ НІКОГО
+// є елемент масиву , який незнає нікого
+// якщо не одна така людина , то вертаємо not found
+// якщо один , то перевірити , чи його знають усі люди, якщо так то вертаємо found
+
+const people1 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+  {
+    name: "Eva",
+    know: ["Alex", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: [],
+  },
+];
+
+const people2 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: ["Eva"],
+  },
+  {
+    name: "Eva",
+    know: [],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+];
+
+const people3 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: [],
+  },
+  {
+    name: "Eva",
+    know: ["Alex", "Jhon"],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+];
