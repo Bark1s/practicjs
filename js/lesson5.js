f0406261b9528d37d7fdc939e199e241144eb3e4
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-03-10 16:44:53 +0200

    add task 5

--- js/lesson5.js
+++ js/lesson5.js
@@ -31,3 +31,54 @@
 //     document.body.classList.toggle("light", this.checked);
 //   });
 // });
+
+// ======task 3-========
+// СТВОРИ СПИСОК ЗАМІТОК НА ДЕНЬ:
+// 1)ПОЛУЧИТИ ДОСТУП ДО ЕЛЕМЕНТІВ ФОРМИ, ПРИ НАТИСКАННЯ НА КНОПОЧКУ ADD
+const form = document.querySelector("#form");
+form.addEventListener("submit", submitHendler);
+const listItem = document.querySelector(".list-input");
+
+function submitHendler(event) {
+  event.preventDefault();
+
+  const input = event.target.elements.question.value;
+  const itemElem = document.createElement("li");
+  itemElem.textContent = input;
+  listItem.appendChild(itemElem);
+  const unParse = JSON.parse(localStorage.getItem("listItems")) || [];
+  unParse.push(input);
+  saveLS(unParse);
+  form.reset();
+}
+
+// 2)НА ОСНОВІ ДАНИХ ЯКІ МИ ВЗЯЛИ З ФОРМИ ВІДМАЛЮВАТИ ЕЛЕМЕНТИ СПИСКУ НА ЕКРАН ЗА ДОПОМОГОЮ КРЕЙТЕЛЕМЕНТ
+
+// 3)ДОДАЙ ЦЕЙ СПИСОК ДО ЛОКАЛ СТОРЕДЖ
+function saveLS(item) {
+  localStorage.setItem("listItems", JSON.stringify(item));
+}
+
+// 4)ДОДАЙ ДОДАТКОВИЙ ФУНКЦІОНАЛ, ЩОБ ПРИ ОНОВЛЕННІ СТОРІНКИ СПИСОК НЕ ВИДАЛЯВСЯ
+function listView() {
+  const getInfo = localStorage.getItem("listItems");
+  if (getInfo) {
+    const parseArr = JSON.parse(getInfo);
+    for (const elem of parseArr) {
+      const itemElem = document.createElement("li");
+      itemElem.textContent = elem;
+      listItem.appendChild(itemElem);
+    }
+  }
+}
+listView();
+
+// 5) СТВОРЮЄМО КНОПОЧКУ, ПРИ ЯКОМУ БУДЕ ОЧИЩАВСЯ ЛОКАЛ СТОРЕДЖ
+
+const btn = document.querySelector(".btn-clear");
+btn.addEventListener("click", fooClear);
+
+function fooClear() {
+  localStorage.removeItem("listItems");
+  listItem.innerHTML = "";
+}
